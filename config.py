d5d11a5a4cf2624ec28e37c20009ecfe6c7aab49
wtflinks <167952966+wtflinks@users.noreply.github.com>
2024-09-16 01:44:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "instahooboo")
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "bn623441bc55ef14be63018992134d393a62a9dfh5")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 600)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "False")
+IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/How_to_Download_7x/35") 
 
 #force sub channel id, if you want enable force sub
